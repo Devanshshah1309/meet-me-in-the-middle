d9d0df3695b231c88704c359d5fd2a29b53ac694
G <garyyansen@gmail.com>
2022-01-07 11:15:40 +0800

    Implement REST api

--- backend/app.py
+++ backend/app.py
@@ -1,12 +1,20 @@
-from flask import Flask
+from flask import Flask, request
+from flask_restful import Api, Resource
 
 app = Flask(__name__)
+api = Api(app)
 
 
-@app.route('/')
-def hello_world():
-    return 'Hello World!'
+class MeetingPoints(Resource):
+    def get(self):
+        query_params = request.args.to_dict(flat=False)
 
+        # run algorithm
+        
+        return {"data": query_params}
+
+
+api.add_resource(MeetingPoints, "/api")
 
 if __name__ == '__main__':
-    app.run()
+    app.run(debug=True)
